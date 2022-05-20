5a560a3df2c055f9270b42d73a60e95d8371116d
eakarr <eaakar.55@gmail.com>
2022-05-21 02:13:11 +0300

    About p tag modified

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,13 +18,25 @@ const About = () => {
 
         <div className="about__content">
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et
-            voluptatem assumenda consectetur a deserunt doloremque ipsa soluta
-            magnam sequi! Cumque hic omnis sed quidem temporibus esse maxime,
-            reprehenderit provident ipsum. Cumque nam perspiciatis esse, quam
-            iusto, inventore enim eaque maxime non reprehenderit nobis nihil ut!
-            Veritatis dolorem fugit sint repudiandae cumque debitis ipsum
-            corporis quam? Voluptatum laborum voluptatem vero fugit.
+            Hi there! My name is Emir. I am a passionate self-taught Frontend
+            Developer. I am a very friendly and social person. I like spending
+            time with my friends and having a good time with them. Actually, my
+            adventure started as an Environmental Engineer. I know, it sounds
+            interesting. I graduated from Hacettepe University as an
+            Environmental Engineer in August 2021. Two months after my
+            graduation, I realized that this job was not meant to be my primary
+            job. So, I decided to follow my main curiosity which was coding! I
+            was always curious about how those web pages were created, how the
+            buttons were put there, how those buttons knew where I want to go or
+            what I want to do, how those paragraphs were written there, etc. So,
+            I decided to search about what this field is called in general and I
+            found out that this was called Frontend Development. I started
+            working by watching videos, reading lots of articles, and of course,
+            making lots of googling. After some time, I got into a couple of
+            bootcamps and improved my Fronted skills even further! My main goal
+            is to become a Frontend Developer that every company needs in their
+            teams! Please, feel free to reach me out anytime! Thanks for reading
+            until the end!
           </p>
 
           <a href="#contact" className="btn btn-primary">
