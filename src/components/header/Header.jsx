1c3aac3e71518788e9b1573cb473ab882f621b28
eakarr <eaakar.55@gmail.com>
2022-05-20 03:44:30 +0300

    Header component modified

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import HeaderButtons from "./headerButtons/HeaderButtons";
+import ME from "../../assests/me.png";
 
 import "./Header.scss";
+import HeaderSocials from "./headerSocials/HeaderSocials";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Emir AKAR</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <HeaderButtons />
+        <HeaderSocials />
 
-export default Header
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
