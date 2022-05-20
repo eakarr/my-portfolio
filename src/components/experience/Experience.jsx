fdc1977c5d312b34cb5538bb27db3ac4097a7569
eakarr <eaakar.55@gmail.com>
2022-05-21 02:13:32 +0300

    Experience component modified

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiJavascript } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { SiCss3 } from "react-icons/si";
+import { SiSass } from "react-icons/si";
 
-import "./Experience.scss"
+import "./Experience.scss";
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
 
-export default Experience
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <RiReactjsLine className="experience__details-icon" />
+              <h4>ReactJS</h4>
+            </article>
+            <article className="experience__details">
+              <SiJavascript className="experience__details-icon" />
+              <h4>JavaScript</h4>
+            </article>
+            <article className="experience__details">
+              <FaHtml5 className="experience__details-icon" />
+              <h4>HTML</h4>
+            </article>
+            <article className="experience__details">
+              <SiCss3 className="experience__details-icon" />
+              <h4>CSS</h4>
+            </article>
+            <article className="experience__details">
+              <SiSass className="experience__details-icon" />
+              <h4>SASS</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
