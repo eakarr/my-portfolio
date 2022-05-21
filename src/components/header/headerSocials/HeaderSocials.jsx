e42a23c2dd04951151ab1d72314bcd02115d1de4
eakarr <eaakar.55@gmail.com>
2022-05-21 15:37:55 +0300

    Social links generated dynamically

--- src/components/header/headerSocials/HeaderSocials.jsx
+++ src/components/header/headerSocials/HeaderSocials.jsx
@@ -1,24 +1,43 @@
-import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 import { BsMedium } from "react-icons/bs";
 
+const socials = [
+  {
+    id: Math.random(),
+    target: "_blank",
+    rel: "noreferrer",
+    href: "https://www.linkedin.com/in/emir-akar/",
+    icon: <BsLinkedin />,
+  },
+  {
+    id: Math.random(),
+    target: "_blank",
+    rel: "noreferrer",
+    href: "https://github.com/eakarr",
+    icon: <BsGithub />,
+  },
+  {
+    id: Math.random(),
+    target: "_blank",
+    rel: "noreferrer",
+    href: "https://medium.com/@eaakar.55",
+    icon: <BsMedium />,
+  },
+];
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://www.linkedin.com/in/emir-akar/"
-      >
-        <BsLinkedin />
-      </a>
-      <a target="_blank" rel="noreferrer" href="https://github.com/eakarr">
-        <BsGithub />
-      </a>
-      <a target="_blank" rel="noreferrer" href="https://medium.com/@eaakar.55">
-        <BsMedium />
-      </a>
+      {socials.map((social) => (
+        <a
+          key={social.id}
+          target={social.target}
+          rel={social.rel}
+          href={social.href}
+        >
+          {social.icon}
+        </a>
+      ))}
     </div>
   );
 };
