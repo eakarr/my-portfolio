02f8f1f7299d64d49b21ab9d3bc2734799c02f77
eakarr <eaakar.55@gmail.com>
2022-05-21 15:37:31 +0300

    Unnecessary spaces deleted

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,11 +14,9 @@ const Header = () => {
         <h5 className="text-light">Frontend Developer</h5>
         <HeaderButtons />
         <HeaderSocials />
-
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-        
         <a href="#contact" className="scroll__down">
           Scroll Down
         </a>
